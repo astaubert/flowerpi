fba585a6dfb1d5fa3fd9588167d4bd9b3a139a62
Andreas Staubert <mbair@staubert.de>
2017-01-15 13:46:28 +0100

    Version V02.00.06

--- fptemp.py
+++ fptemp.py
@@ -79,7 +79,7 @@ def getserial():
 		f = open('/proc/cpuinfo','r')
 		for line in f:
 			if line[0:6]=='Serial':
-		       cpuserial = line[10:26]
+				cpuserial = line[10:26]
 		f.close()
 	except:
 		cpuserial = "ERROR000000000"
